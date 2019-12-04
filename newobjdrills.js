220322f562f47bd783a63efd682e17723692446d
Taylor Bradshaw <taylordyoung1@gmail.com>
2019-12-04 07:36:09 -0500

    added fillerWords object and for loop

--- newobjdrills.js
+++ newobjdrills.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// exercise 1
+
 let loaf = {
     flour: 300, 
     water: 210, 
@@ -13,3 +15,16 @@ console.log(loaf);
 
 console.log(loaf.hydration());
 
+//exercise 2
+
+const fillerWords = {
+    foo: 'fools',
+    bar: 'bar hopping',
+    fum: 'fee fi fo fum',
+    quux: 'we are at the quux of this debate',
+    spam: 'why do you exist?',
+}
+
+for (const key in fillerWords) {
+    console.log(`${key}, ${fillerWords[key]}`);
+}
